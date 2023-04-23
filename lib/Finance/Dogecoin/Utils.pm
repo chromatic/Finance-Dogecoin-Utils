37a7342ef75a2a0cb976bea7a51a1fb2195cdd9c
chromatic <chromatic@wgz.org>
2023-04-23 16:31:28 -0700

    Add utility functions to Finance::Dogecoin::Utils

--- lib/Finance/Dogecoin/Utils.pm
+++ lib/Finance/Dogecoin/Utils.pm
@@ -4,6 +4,23 @@ package Finance::Dogecoin::Utils;
 use strict;
 use warnings;
 
+use File::HomeDir;
+use Path::Tiny;
+
+use Exporter::Shiny our @EXPORT = qw( get_conf_dir get_auth_file get_dogecoin_conf_dir );
+
+sub get_conf_dir {
+    return path(File::HomeDir->my_data)->child('dogeutils')->mkdir;
+}
+
+sub get_auth_file {
+    return get_conf_dir()->child( 'auth.json' );
+}
+
+sub get_dogecoin_conf_dir {
+    return path(File::HomeDir->my_home)->child('.dogecoin')->child('backups');
+}
+
 1;
 
 __END__
